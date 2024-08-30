d8d98f5a1256d00bb3c5bba63018da8d4d6a8b49
Kaleigh Scruggs <kaleighscruggs@gmail.com>
2024-08-30 10:40:18 -0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,76 @@
+import { animate, style, transition, trigger } from '@angular/animations';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('500ms', style({ opacity: 1 })),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
-  title = 'age-calculator';
+  ageForm: FormGroup;
+  age: { years: number; months: number; days: number } | null = null;
+
+  constructor(private fb: FormBuilder) {
+    this.ageForm = this.fb.group({
+      day: ['', [Validators.required, Validators.min(1), Validators.max(31)]],
+      month: ['', [Validators.required, Validators.min(1), Validators.max(12)]],
+      year: [
+        '',
+        [Validators.required, Validators.max(new Date().getFullYear())],
+      ],
+    });
+  }
+
+  calculateAge() {
+    if (this.ageForm.invalid) {
+      return;
+    }
+
+    const { day, month, year } = this.ageForm.value;
+
+    const birthDate = new Date(year, month - 1, day);
+    const currentDate = new Date();
+
+    if (birthDate > currentDate || isNaN(birthDate.getTime())) {
+      this.ageForm.setErrors({ invalidDate: true });
+      return;
+    }
+
+    let years = currentDate.getFullYear() - birthDate.getFullYear();
+    let months = currentDate.getMonth() - birthDate.getMonth();
+    let days = currentDate.getDate() - birthDate.getDate();
+
+    if (days < 0) {
+      months--;
+      days += new Date(
+        currentDate.getFullYear(),
+        currentDate.getMonth(),
+        0
+      ).getDate();
+    }
+
+    if (months < 0) {
+      years--;
+      months += 12;
+    }
+
+    this.age = { years, months, days };
+  }
 }
